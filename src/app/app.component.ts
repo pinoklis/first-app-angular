5c6483467a4b554d7259a20bbb10237a8b50a79d
Thimios Kardaras <kardarasthimios@gmail.com>
2025-01-25 15:00:43 +0200

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from "../task/task.component";
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
