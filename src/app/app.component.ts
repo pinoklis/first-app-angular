85bc16d5cd82ed9da5f133b8b02ddef863d50e82
Thimios Kardaras <kardarasthimios@gmail.com>
2025-01-25 14:41:22 +0200

    udemy courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "../task/task.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // standalone : true, Angular >=19 einai panta true kai den xreiazetai na oristei
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first-app';
+
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id : string) {
+    // console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
+  }
+
 }
